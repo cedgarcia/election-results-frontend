9ec8fdc477bafcd65482751a7f87d9d4a25d6b7b
Cedrick Garcia <icedgarcia@gmail.com>
2025-08-04 13:20:57 +0800

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ const PhilippinesMapPage = () => {
   return (
     <div className="mx-auto max-w-6xl p-4">
       <div className="overflow-hidden rounded-lg border shadow-lg">
-        test ulit
+        test ulit 3rd time
         <PhilippinesMap
           onAreaClick={handleAreaClick}
           selectedArea={selectedArea}
